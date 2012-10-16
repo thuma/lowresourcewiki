e26d7e577baa33f01610203fd8ad5d611ecd5701
Martin Thuresson <martin.thure@gmail.com>
2012-10-16 21:05:46 +0200

    Corrected Typos and sys error.

--- save.php
+++ save.php
@@ -5,20 +5,17 @@ $_POST["wikiform"]
 $_POST["page"]
 $_POST["version"]
 */
-if (is_file("wikidata".$_POST["page"].".js"){
+if (is_file("wikidata".$_POST["page"].".js")){
 file_get_contents("wikidata".$_POST["page"].".js", $oldpage);
 }
 if (is_file("wikidata".$_POST["page"].".js.".$_POST["version"])){
 echo "Incorrect version no.";
 } else {
 file_put_contents("wikidata".$_POST["page"].".js.".$_POST["version"], $oldpage);
-$newpage = 'var wikibody = "'. $_POST["wikiform"]. '";\n';
-$newpage = $newpage . 'var version = "'.strval(intval($_POST["version"])+1).'";'
+$newpage = 'var wikibody = "'. $_POST["wikiform"]. '";'."\n";
+$newpage = $newpage . 'var version = "'.strval(intval($_POST["version"])+1).'";';
 file_put_contents("wikidata".$_POST["page"].".js", $newpage);
 echo "Stored.";
 }
-
-
-
-
+echo "End";
 ?>
