001758d4a95f745a732a66b776242522686d7f64
Martin Thuresson <martin.thure@gmail.com>
2012-10-16 20:52:33 +0200

    Correted page creation.

--- save.php
+++ save.php
@@ -5,8 +5,9 @@ $_POST["wikiform"]
 $_POST["page"]
 $_POST["version"]
 */
-
+if (is_file("wikidata".$_POST["page"].".js"){
 file_get_contents("wikidata".$_POST["page"].".js", $oldpage);
+}
 if (is_file("wikidata".$_POST["page"].".js.".$_POST["version"])){
 echo "Incorrect version no.";
 } else {
@@ -14,6 +15,7 @@ file_put_contents("wikidata".$_POST["page"].".js.".$_POST["version"], $oldpage);
 $newpage = 'var wikibody = "'. $_POST["wikiform"]. '";\n';
 $newpage = $newpage . 'var version = "'.strval(intval($_POST["version"])+1).'";'
 file_put_contents("wikidata".$_POST["page"].".js", $newpage);
+echo "Stored.";
 }
 
 
