81eba0a2b2bffdb4356662e254a476965ea3fd18
Martin Thuresson <martin.thure@gmail.com>
2012-10-16 21:33:24 +0200

    Corrected data.

--- save.php
+++ save.php
@@ -15,7 +15,7 @@ if (is_file("wikidata".$_POST["page"].".js.".$_POST["version"])){
 echo "Page is changed after you started to edit it.<br /> Go back to uppdate and add your changed again.<br />";
 } else {
 file_put_contents("wikidata".$_POST["page"].".js.".$_POST["version"], $oldpage);
-$newpage = 'var wikibody = "'.str_replace("\"","\\\"",str_replace(array("\r\n", "\n", "\r"), "\\n", $_POST["wikiform"])).'";'."\n";
+$newpage = 'var wikibody = "'.str_replace("\"","\\\"",str_replace(array("\r\n", "\n", "\r"), "\\n", str_replace("\\","\\\\",$_POST["wikiform"])).'";'."\n";
 $newpage = $newpage . 'var version = "'.strval(intval($_POST["version"])+1).'";';
 file_put_contents("wikidata".$_POST["page"].".js", $newpage);
 echo "Stored OK <br />";
