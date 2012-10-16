62482679a74c8cc713b8c65425c5fbabccb3fe36
Martin Thuresson <martin.thure@gmail.com>
2012-10-16 20:45:35 +0200

    Added store support.

---
+++ save.php
@@ -0,0 +1,22 @@
+<?php 
+
+/*
+$_POST["wikiform"]
+$_POST["page"]
+$_POST["version"]
+*/
+
+file_get_contents("wikidata".$_POST["page"].".js", $oldpage);
+if (is_file("wikidata".$_POST["page"].".js.".$_POST["version"])){
+echo "Incorrect version no.";
+} else {
+file_put_contents("wikidata".$_POST["page"].".js.".$_POST["version"], $oldpage);
+$newpage = 'var wikibody = "'. $_POST["wikiform"]. '";\n';
+$newpage = $newpage . 'var version = "'.strval(intval($_POST["version"])+1).'";'
+file_put_contents("wikidata".$_POST["page"].".js", $newpage);
+}
+
+
+
+
+?>
--- wikidataMain.js
+++ wikidataMain.js
@@ -1,1 +1,2 @@
 var wikibody = "Hej! Skoj Detta! [[Start]] [img:1234.jpg]";
+var version = "1";
