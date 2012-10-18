5e55882ac4d22fd8f7f51f33f8d8dd68295e16bb
Martin Thuresson <martin.thure@gmail.com>
2012-10-18 20:47:27 +0200

    Added diff line brakes.

--- wikidataMain.js
+++ wikidataMain.js
@@ -1,2 +1,2 @@
-var wikibody = "Hej! Skoj Detta! [[Start]] [img:1234.jpg]";
+var wikibody = "Hej! Skoj Detta! [[Start]] [img:1234.jpg] \n Hej! sadkjfhaskjfshdkl asdfh as \n ksdfjhasdkjfh \n asdflgasdfjg  \n sadfhgas dkfjhg";
 var version = "1";
